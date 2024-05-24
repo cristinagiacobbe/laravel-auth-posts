c84608c74843032d2cdc06dbddb8e16054177074
cristinagiacobbe <crigiak@gmail.com>
2024-05-24 23:10:37 +0200

    Add upload resource controller metod

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mt-3">Add a new comic</h1>
+        <h1 class="mt-3">Add a new post</h1>
 
         @include('partials.errors')
 
@@ -12,8 +12,8 @@
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control @error('title')is-invalid @enderror" name="title" id="title"
-                    aria-describedby="helptitle" placeholder="Type a title for your comic" />
-                <small id="helptitle" class="form-text text-muted">Type comic title</small>
+                    aria-describedby="helptitle" placeholder="Type a title for your post" value="{{ old('title') }}" />
+                <small id="helptitle" class="form-text text-muted">Type post title</small>
 
                 @error('title')
                     <div class="alert alert-danger ">{{ $message }}</div>
@@ -36,11 +36,11 @@
 
             <div class="mb-3">
                 <label for="content" class="form-label"></label>
-                <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+                <textarea class="form-control" name="content" id="content" rows="3">{{ old('content') }}</textarea>
             </div>
 
             <button class="btn btn-primary" type="submit">Create</button>
-            <button class="btn btn-danger">Turn back to comic list</button>
+            <button class="btn btn-danger">Turn back to post list</button>
 
         </form>
     </div>
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,32 +2,47 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mt-3">Add a new comic</h1>
+        <h1 class="mt-3">Update post n.{{ $post->id }}</h1>
 
-        <form action="{{ route('admin.posts.store') }}" method="post">
+        @include('partials.errors')
+
+        <form action="{{ route('admin.posts.update', $post) }}" method="post" enctype="multipart/form-data">
             @csrf
+            @method('PUT')
 
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title" id="title" aria-describedby="helptitle"
-                    placeholder="Type a title for your comic" />
-                <small id="helptitle" class="form-text text-muted">Type comic title</small>
+                <input type="text" class="form-control @error('title')is-invalid @enderror" name="title" id="title"
+                    aria-describedby="helptitle" placeholder="Type here to change post title"
+                    value="{{ old('title') ?: $post->title }}" />
+                <small id="helptitle" class="form-text text-muted">Update post title</small>
+
+                @error('title')
+                    <div class="alert alert-danger ">{{ $message }}</div>
+                @enderror
+
             </div>
 
             <div class="mb-3">
                 <label for="cover_image" class="form-label">Cover image</label>
-                <input type="file" enctype="multipart/form-data" class="form-control" name="cover_image" id="cover_image"
-                    aria-describedby="helpcover_image" placeholder="Here you can upload an image for your comic" />
+                <input type="file" class="form-control @error('cover_image')is-invalid @enderror" name="cover_image"
+                    id="cover_image" aria-describedby="helpcover_image"
+                    placeholder="Here you can upload an image for your comic" />
                 <small id="helpcover_image" class="form-text text-muted">Upload cover_image</small>
+
+                @error('cover_image')
+                    <div class="alert alert-danger ">{{ $message }}</div>
+                @enderror
             </div>
 
 
             <div class="mb-3">
                 <label for="content" class="form-label"></label>
-                <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+                <textarea class="form-control" name="content" id="content" rows="3">{{ old('content') ?: $post->content }}</textarea>
             </div>
 
-            <button type="submit">Create</button>
+            <button class="btn btn-primary" type="submit">Update</button>
+            <button class="btn btn-danger">Turn back to comic list</button>
 
         </form>
     </div>
